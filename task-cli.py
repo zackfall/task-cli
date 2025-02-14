0ff82837b1a0c1c229dcc3611e7c547638515360
Isaac Zamora <zackfall@outlook.com>
2025-02-14 08:52:20 -0500

    Function to create the storage file

---
+++ task-cli.py
@@ -0,0 +1,39 @@
+import json
+from typing import List
+
+
+class Task:
+    def __init__(
+        self, id: int, description: str, status: str, createdAt: str, updatedAt: str
+    ):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+
+    def to_dict(self):
+        return self.__dict__
+
+
+class Storage:
+    def __init__(self, version: str, tasks: List[Task]):
+        self.version = version
+        self.tasks = tasks
+
+    def to_dict(self):
+        return {
+            "version": self.version,
+            "tasks": [task.to_dict() for task in self.tasks],
+        }
+
+
+def create_storage(path: str, version: str):
+    storage = Storage(version, []).to_dict()
+    str_json = json.dumps(storage)
+    with open(path, "wt") as file:
+        file.write(str_json)
+
+
+if __name__ == "__main__":
+    create_storage("./storage.json", "0.0.1")
